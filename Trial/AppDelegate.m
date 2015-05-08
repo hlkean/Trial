423afff64c91e9e26306d2759c22341c91c2158a
hlkean <hlkean@gmail.com>
2015-05-07 20:17:17 -0400

    Beta 8

--- Trial/AppDelegate.m
+++ Trial/AppDelegate.m
@@ -5,41 +5,66 @@
 //  Created by Henry Kean on 4/27/15.
 //  Copyright (c) 2015 Henry Kean. All rights reserved.
 //
-
 #import "AppDelegate.h"
+#import <Spotify/Spotify.h>
+#import "Config.h"
+#import "ViewController.h"
+// Constants
+static NSString * const kClientId = @"58bb7b43ed924142b073d0b350fb4fd5";
+static NSString * const kCallbackURL = @"trial://callback";
+static NSString * const kTokenSwapURL = @"http://localhost:1234/swap";
+
 
 @interface AppDelegate ()
-
+@property (nonatomic, strong) SPTSession *session;
+@property (nonatomic, strong) SPTAudioStreamingController *player;
 @end
 
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Set up shared authentication information
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    auth.clientID = @kClientId;
+    auth.requestedScopes = @[SPTAuthStreamingScope];
+    auth.redirectURL = [NSURL URLWithString:@kCallbackURL];
+#ifdef kTokenSwapServiceURL
+    auth.tokenSwapURL = [NSURL URLWithString:@kTokenSwapServiceURL];
+#endif
+#ifdef kTokenRefreshServiceURL
+    auth.tokenRefreshURL = [NSURL URLWithString:@kTokenRefreshServiceURL];
+#endif
+    auth.sessionUserDefaultsKey = @kSessionUserDefaultsKey;
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    
+    SPTAuthCallback authCallback = ^(NSError *error, SPTSession *session) {
+        // This is the callback that'll be triggered when auth is completed (or fails).
+        
+        if (error != nil) {
+            NSLog(@"*** Auth error: %@", error);
+            return;
+        }
+        
+        auth.session = session;
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"sessionUpdated" object:self];
+    };
+    
+    /*
+     Handle the callback from the authentication service. -[SPAuth -canHandleURL:]
+     helps us filter out URLs that aren't authentication URLs (i.e., URLs you use elsewhere in your application).
+     */
+    
+    if ([auth canHandleURL:url]) {
+        [auth handleAuthCallbackWithTriggeredAuthURL:url callback:authCallback];
+        return YES;
+    }
+    
+    return NO;
 }
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 @end
+
